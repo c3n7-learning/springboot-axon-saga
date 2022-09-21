fa3b1692737587c7806a1758903c98188e0d4088
Timothy Karani <kartimothy@gmail.com>
2022-09-21 13:15:10 +0300

    Added api gateway auto locating

---
+++ 4-api-gateway/src/main/java/tech/c3n7/apigateway/ApiGatewayApplication.java
@@ -0,0 +1,16 @@
+package tech.c3n7.apigateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+
+
+@EnableDiscoveryClient
+@SpringBootApplication
+public class ApiGatewayApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiGatewayApplication.class, args);
+	}
+
+}
