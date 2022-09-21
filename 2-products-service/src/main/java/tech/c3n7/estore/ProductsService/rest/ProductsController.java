993d352149bcaaee547d9f1fb11af817edc68b40
Timothy Karani <kartimothy@gmail.com>
2022-09-21 16:56:34 +0300

    Load balancing

--- 2-products-service/src/main/java/tech/c3n7/estore/ProductsService/rest/ProductsController.java
+++ 2-products-service/src/main/java/tech/c3n7/estore/ProductsService/rest/ProductsController.java
@@ -1,10 +1,14 @@
 package tech.c3n7.estore.ProductsService.rest;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.env.Environment;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/products")
 public class ProductsController {
+    @Autowired
+    private Environment environment;
     @PostMapping
     public String createProduct() {
         return "HTTP POST Handled!";
@@ -12,7 +16,7 @@ public class ProductsController {
 
     @GetMapping
     public String getProduct() {
-        return "HTTP GET Handled";
+        return "HTTP GET Handled:: " +environment.getProperty("local.server.port");
     }
 
     @PutMapping
