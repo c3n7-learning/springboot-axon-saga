809fa609863981e6757602d213aee077b145eadb
Timothy Karani <kartimothy@gmail.com>
2022-09-22 13:10:39 +0300

    First of many

---
+++ 7-products-service/src/main/java/tech/c3n7/estore/ProductsService/rest/ProductsController.java
@@ -0,0 +1,31 @@
+package tech.c3n7.estore.ProductsService.rest;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.env.Environment;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/products")
+public class ProductsController {
+    @Autowired
+    private Environment environment;
+    @PostMapping
+    public String createProduct() {
+        return "HTTP POST Handled!";
+    }
+
+    @GetMapping
+    public String getProduct() {
+        return "HTTP GET Handled:: " +environment.getProperty("local.server.port");
+    }
+
+    @PutMapping
+    public String updateProduct() {
+        return "HTTP PUT Handled";
+    }
+
+    @DeleteMapping
+    public String deleteProduct() {
+        return "HTTP DELETE Handled";
+    }
+}
