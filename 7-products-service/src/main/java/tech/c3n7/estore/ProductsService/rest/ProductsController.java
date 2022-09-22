edd9ea4a9ad1adc4333f25936883ee43f162b6b1
Timothy Karani <kartimothy@gmail.com>
2022-09-22 13:59:31 +0300

    Added command2

--- 7-products-service/src/main/java/tech/c3n7/estore/ProductsService/rest/ProductsController.java
+++ 7-products-service/src/main/java/tech/c3n7/estore/ProductsService/rest/ProductsController.java
@@ -1,22 +1,49 @@
 package tech.c3n7.estore.ProductsService.rest;
 
+import org.axonframework.commandhandling.gateway.CommandGateway;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.core.env.Environment;
 import org.springframework.web.bind.annotation.*;
+import tech.c3n7.estore.ProductsService.command.CreateProductCommand;
+
+import java.util.UUID;
 
 @RestController
 @RequestMapping("/products")
 public class ProductsController {
+
+    private final Environment environment;
+    private final CommandGateway commandGateway;
+
     @Autowired
-    private Environment environment;
+    private ProductsController(Environment environment, CommandGateway commandGateway) {
+        this.environment = environment;
+        this.commandGateway = commandGateway;
+    }
+
     @PostMapping
     public String createProduct(@RequestBody CreateProductRestModel createProductRestModel) {
-        return "HTTP POST Handled!:: " + createProductRestModel.getTitle();
+
+        CreateProductCommand createProductCommand = CreateProductCommand.builder()
+                .price(createProductRestModel.getPrice())
+                .quantity(createProductRestModel.getQuantity())
+                .title(createProductRestModel.getTitle())
+                .productId(UUID.randomUUID().toString())
+                .build();
+
+        String returnValue;
+        try {
+            returnValue = commandGateway.sendAndWait(createProductCommand);
+        } catch (Exception ex) {
+            returnValue = ex.getLocalizedMessage();
+        }
+
+        return returnValue;
     }
 
     @GetMapping
     public String getProduct() {
-        return "HTTP GET Handled:: " +environment.getProperty("local.server.port");
+        return "HTTP GET Handled:: " + environment.getProperty("local.server.port");
     }
 
     @PutMapping
