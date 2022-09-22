8d03801853245faf2bc8e1ea328f605f3c004734
Timothy Karani <kartimothy@gmail.com>
2022-09-22 14:40:30 +0300

    Can publish events

--- 7-products-service/src/main/java/tech/c3n7/estore/ProductsService/command/ProductAggregate.java
+++ 7-products-service/src/main/java/tech/c3n7/estore/ProductsService/command/ProductAggregate.java
@@ -1,12 +1,24 @@
 package tech.c3n7.estore.ProductsService.command;
 
 import org.axonframework.commandhandling.CommandHandler;
+import org.axonframework.eventsourcing.EventSourcingHandler;
+import org.axonframework.modelling.command.AggregateIdentifier;
+import org.axonframework.modelling.command.AggregateLifecycle;
 import org.axonframework.spring.stereotype.Aggregate;
+import org.springframework.beans.BeanUtils;
+import tech.c3n7.estore.ProductsService.core.events.ProductCreatedEvent;
 
 import java.math.BigDecimal;
 
 @Aggregate
 public class ProductAggregate {
+
+    @AggregateIdentifier
+    private String productId;
+    private String title;
+    private BigDecimal price;
+    private Integer quantity;
+
     public ProductAggregate() {
     }
 
@@ -21,5 +33,18 @@ public class ProductAggregate {
                 || createProductCommand.getTitle().isBlank()) {
             throw new IllegalArgumentException("Price cannot be less or equal than zero");
         }
+
+        ProductCreatedEvent productCreatedEvent = new ProductCreatedEvent();
+        BeanUtils.copyProperties(createProductCommand, productCreatedEvent);
+
+        AggregateLifecycle.apply(productCreatedEvent);
+    }
+
+    @EventSourcingHandler
+    public void on(ProductCreatedEvent productCreatedEvent) {
+        this.productId = productCreatedEvent.getProductId();
+        this.price = productCreatedEvent.getPrice();
+        this.title = productCreatedEvent.getTitle();
+        this.quantity = productCreatedEvent.getQuantity();
     }
 }
