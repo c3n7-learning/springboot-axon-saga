9d2fb9c22dc4e829a4cfc23031d67a73f382b31c
Timothy Karani <kartimothy@gmail.com>
2022-09-29 22:50:36 +0300

    handling command execution exception

--- 14-products-service/src/main/java/tech/c3n7/estore/ProductsService/command/ProductAggregate.java
+++ 14-products-service/src/main/java/tech/c3n7/estore/ProductsService/command/ProductAggregate.java
@@ -23,7 +23,7 @@ public class ProductAggregate {
     }
 
     @CommandHandler
-    public ProductAggregate(CreateProductCommand createProductCommand) {
+    public ProductAggregate(CreateProductCommand createProductCommand) throws Exception {
         // Validate Create Product Command
         if (createProductCommand.getPrice().compareTo(BigDecimal.ZERO) <= 0) {
             throw new IllegalArgumentException("Price cannot be less or equal than zero");
@@ -38,6 +38,8 @@ public class ProductAggregate {
         BeanUtils.copyProperties(createProductCommand, productCreatedEvent);
 
         AggregateLifecycle.apply(productCreatedEvent);
+
+        if(true) throw new Exception("An error took place in the CreateProductCommand @CommandHandler");
     }
 
     @EventSourcingHandler
