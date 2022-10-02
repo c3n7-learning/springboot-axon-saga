30cc50c98588f5bd21a028f177d50bb5794334cf
Timothy Karani <kartimothy@gmail.com>
2022-10-02 08:33:03 +0300

    Added saga class

---
+++ 15-orders-service/src/main/java/tech/c3n7/OrdersService/saga/OrderSaga.java
@@ -0,0 +1,11 @@
+package tech.c3n7.OrdersService.saga;
+
+import org.axonframework.commandhandling.gateway.CommandGateway;
+import org.axonframework.spring.stereotype.Saga;
+import org.springframework.beans.factory.annotation.Autowired;
+
+@Saga
+public class OrderSaga {
+    @Autowired
+    private transient CommandGateway commandGateway;
+}
