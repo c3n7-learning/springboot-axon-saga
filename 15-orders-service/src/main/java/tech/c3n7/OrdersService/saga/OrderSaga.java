3f6773589bca49775573514af72d8b6614d02eff
Timothy Karani <kartimothy@gmail.com>
2022-10-02 08:54:54 +0300

    Added core module

--- 15-orders-service/src/main/java/tech/c3n7/OrdersService/saga/OrderSaga.java
+++ 15-orders-service/src/main/java/tech/c3n7/OrdersService/saga/OrderSaga.java
@@ -1,11 +1,20 @@
 package tech.c3n7.OrdersService.saga;
 
 import org.axonframework.commandhandling.gateway.CommandGateway;
+import org.axonframework.modelling.saga.SagaEventHandler;
+import org.axonframework.modelling.saga.StartSaga;
 import org.axonframework.spring.stereotype.Saga;
 import org.springframework.beans.factory.annotation.Autowired;
+import tech.c3n7.OrdersService.core.events.OrderCreatedEvent;
 
 @Saga
 public class OrderSaga {
     @Autowired
     private transient CommandGateway commandGateway;
+
+    @StartSaga
+    @SagaEventHandler(associationProperty = "orderId")
+    public void handle(OrderCreatedEvent orderCreatedEvent) {
+
+    }
 }
